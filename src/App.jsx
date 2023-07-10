ea8f9d4ebd6f55c779f5d9a82641f942a1684eb8
Kade <kadekorn@gmail.com>
2023-07-09 18:50:26 -0700

    Learnt about Router and now I will learn how to do it dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 
-import React, { useState, useEffect } from "react";
+
+import Nav from "./components/Nav.jsx";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 // import Counter from "./components/Counter";
 
 function App() {
-  return (<div>
+  return (
     <Router>
-      <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-      </nav>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact  />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
-  </div>
-  )
+  );
 }
 
 export default App;

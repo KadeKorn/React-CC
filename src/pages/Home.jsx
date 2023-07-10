b8d25232670d2fa59fef2f9d0399c5b6051eae8f
Kade <kadekorn@gmail.com>
2023-07-10 08:02:12 -0700

    Easy fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,25 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Home() {
-    const [users, setUsers] = useState([   ])
+  const [users, setUsers] = useState([]);
 
   async function fetchUsers() {
     const { data } = await axios.get(
       "https://jsonplaceholder.typicode.com/users"
     );
-    setUsers(data)
+    setUsers(data);
+    console.log(data)
   }
 
   useEffect(() => {
-    fetchUsers();
+    setTimeout(() => {
+      fetchUsers();
+    }, 2000);
   }, []);
-  return <h1>{users.length > 0 && users[0].name}</h1>;
+
+  return (
+    <>{users.length > 0 ? <h1>{users[0]?.name}</h1> : <h1>Loading...</h1>}</>
+  );
 }
 
 export default Home;

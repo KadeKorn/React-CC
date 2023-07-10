b367b576b6321d859ef6982efbddf39a64c37196
Kade <kadekorn@gmail.com>
2023-07-09 19:09:45 -0700

    finished up with routers. Learned how to make as many dynamic pages as I want.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Nav from "./components/Nav.jsx";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+import Users from "./pages/User.jsx";
 // import Counter from "./components/Counter";
 
 function App() {
@@ -14,8 +14,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/users/:username" element={<Users />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/User.jsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+
+function Users() {
+  const { username } = useParams();
+
+  return <h1>{username}</h1>;
+}
+
+export default Users;
